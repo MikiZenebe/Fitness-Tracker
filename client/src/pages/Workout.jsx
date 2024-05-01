55d759c0d5bf286e1175220ac16f689551f64421
MikiZenebe <mickyaszenebe@gmail.com>
2024-04-30 23:08:40 -0500

    ui-done

--- client/src/pages/Workout.jsx
+++ client/src/pages/Workout.jsx
@@ -1,3 +1,38 @@
+import {
+  WorkoutCardWrapper,
+  WorkoutContainer,
+  WorkoutLeft,
+  WorkoutRight,
+  WorkoutSecTitle,
+  WorkoutSection,
+  WorkoutTitle,
+  WorkoutWrapper,
+} from "../styles/styles";
+import WorkoutCard from "../components/cards/WorkoutCard";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateCalendar } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 export default function Workout() {
-  return <div>Workout</div>;
+  return (
+    <WorkoutContainer>
+      <WorkoutWrapper>
+        <WorkoutLeft>
+          <WorkoutTitle>Select Date</WorkoutTitle>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar />
+          </LocalizationProvider>
+        </WorkoutLeft>
+        <WorkoutRight>
+          <WorkoutSection>
+            <WorkoutSecTitle>Todays Workout</WorkoutSecTitle>
+            <WorkoutCardWrapper>
+              <WorkoutCard /> <WorkoutCard /> <WorkoutCard /> <WorkoutCard />{" "}
+              <WorkoutCard />
+            </WorkoutCardWrapper>
+          </WorkoutSection>
+        </WorkoutRight>
+      </WorkoutWrapper>
+    </WorkoutContainer>
+  );
 }
--- client/src/styles/styles.js
+++ client/src/styles/styles.js
@@ -395,3 +395,68 @@ export const Details = styled.div`
   align-items: center;
   gap: 6px;
 `;
+
+//Workout Page
+export const WorkoutContainer = styled.div`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 22px 0px;
+  overflow-y: scroll;
+`;
+export const WorkoutWrapper = styled.div`
+  flex: 1;
+  max-width: 1600px;
+  display: flex;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+    flex-direction: column;
+  }
+`;
+export const WorkoutLeft = styled.div`
+  flex: 0.2;
+  height: fit-content;
+  padding: 18px;
+  border: 1px solid ${({ theme }) => theme.text_primary + 20};
+  border-radius: 14px;
+  box-shadow: 1px 6px 20px 0px ${({ theme }) => theme.primary + 15};
+`;
+export const WorkoutTitle = styled.div`
+  font-weight: 600;
+  font-size: 16px;
+  color: ${({ theme }) => theme.primary};
+  @media (max-width: 600px) {
+    font-size: 14px;
+  }
+`;
+export const WorkoutRight = styled.div`
+  flex: 1;
+`;
+export const WorkoutSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 16px;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+export const WorkoutSecTitle = styled.div`
+  font-size: 22px;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+`;
+export const WorkoutCardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  margin-bottom: 100px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
