967065b498c58292d64c15352e10c9e76d7bedc3
MikiZenebe <mickyaszenebe@gmail.com>
2024-05-02 11:37:49 -0500

    redux-setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,20 @@ import "./index.css";
 import { ThemeProvider } from "styled-components";
 import { lightTheme } from "./utils/Themes.js";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <ThemeProvider theme={lightTheme}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
