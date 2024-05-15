9493c671e386e86bbd2cc472c90b661948a1cd48
MikiZenebe <mickyaszenebe@gmail.com>
2024-05-15 11:53:28 -0500

    updated

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://fitness-tracker-nc97.onrender.com/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
